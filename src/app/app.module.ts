47bc4d78d0941cffa1382201088c567caa58e0a5
bndynet <zb@bndy.net>
2023-12-04 17:10:31 +0800

    update to latest angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,6 @@ import { AnimationsIndexComponent } from './animations';
     AnimationsTimerComponent,
     AnimationsIndexComponent,
   ],
-  entryComponents: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
