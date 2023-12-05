058b92144cd5d0e75a208bfc35e5bb8207a9ed9a
bndynet <zb@bndy.net>
2023-12-05 13:49:06 +0800

    google charts

--- src/app/mdw/chart.component.ts
+++ src/app/mdw/chart.component.ts
@@ -1,11 +1,38 @@
 import { Component, Input } from '@angular/core';
+import '@google-web-components/google-chart';
 
 @Component({
     selector: 'chart',
-    template: `<mdw-chart style="width: 300px;" [type]="type" [data]="data" [options]="options"></mdw-chart>`,
+    template: `
+    <mdw-chart style="width: 300px;" [type]="type" [data]="data" [options]='options'></mdw-chart>
+    <span [style.background-color]="item.color" style="color: #fff;" *ngFor="let item of selectedItems">{{item.text}}</span>
+    <hr />
+    <h2 class="text-3xl my-4">Google Charts</h2>
+    <google-chart [data]="googleData"></google-chart>
+    `,
 })
 export class MdwChartComponent {
-    @Input() type?: string;
-    @Input() data: any;
-    @Input() options: any;
+    selectedItems: any[] = [];
+    type = 'donut';
+    data = {
+        'Brunei Darussalam': 739,
+        Jordan: 763,
+        'Burkina Faso': 604,
+        'Cook Islands': 509,
+        Monaco: 205,
+        Switzerland: 108,
+    };
+    options = {
+        innerRadius: '70%',
+        legend: {
+            itemSelectable: true,
+            onItemClick: (e: any) => {
+                console.log(e);
+                this.selectedItems = e.chart.legend.selectedItems;
+            }
+        }
+    };
+
+    googleData = [["Month", "Days"], ["Jan", 31]];
 }
+
--- src/app/mdw/index.component.ts
+++ src/app/mdw/index.component.ts
@@ -4,30 +4,10 @@ import { Component } from '@angular/core';
     selector: 'mdw',
     template: `<div>
     <h1 class="text-4xl my-4">Charts</h1>
-    <chart type="donut" [data]="data" [options]="options"></chart>
-    <span [style.background-color]="item.color" style="color: #fff;" *ngFor="let item of selectedItems">{{item.text}}</span>
-    <hr />
+    <chart></chart>
+
     </div>
     `,
 })
 export class MdwComponent {
-    selectedItems: any[] = [];
-    data = {
-        'Brunei Darussalam': 739,
-        Jordan: 763,
-        'Burkina Faso': 604,
-        'Cook Islands': 509,
-        Monaco: 205,
-        Switzerland: 108,
-    };
-    options = {
-        innerRadius: '70%',
-        legend: {
-            itemSelectable: true,
-            onItemClick: (e: any) => {
-                console.log(e);
-                this.selectedItems = e.chart.legend.selectedItems;
-            }
-        }
-    }
 }
