1ceed20892ef38c783f51746bd8d48092339c5fd
Bendy Zhang <zb@bndy.net>
2024-03-06 14:42:13 +0800

    feat(charts): mdw

--- src/app/mdw/chart.component.ts
+++ src/app/mdw/chart.component.ts
@@ -2,37 +2,86 @@ import { Component, Input } from '@angular/core';
 import '@google-web-components/google-chart';
 
 @Component({
-    selector: 'chart',
-    template: `
-    <mdw-chart style="width: 300px;" [type]="type" [data]="data" [options]='options'></mdw-chart>
-    <span [style.background-color]="item.color" style="color: #fff;" *ngFor="let item of selectedItems">{{item.text}}</span>
+  selector: 'chart',
+  template: `
+    <div class="columns-2 mb-2">
+      <div>
+        <mdw-chart
+          style="width: 300px;"
+          type="pie"
+          [data]="data"
+          [options]="defaultOptions"
+        ></mdw-chart>
+      </div>
+      <div>
+        <div>
+          <mdw-chart
+            style="width: 300px;"
+            [type]="type"
+            [data]="data"
+            [options]="options"
+          ></mdw-chart>
+          <span
+            class="rounded px-2 py-1 ml-2 text-xs"
+            [style.background-color]="item.color"
+            style="color: #fff;"
+            *ngFor="let item of selectedItems"
+            >{{ item.text }}</span
+          >
+        </div>
+      </div>
+    </div>
     <hr />
     <h2 class="text-3xl my-4">Google Charts</h2>
-    <google-chart [data]="googleData"></google-chart>
-    `,
+    <google-chart type="pie" [cols]="googleData.cols" [rows]="googleData.rows"></google-chart>
+  `,
 })
 export class MdwChartComponent {
-    selectedItems: any[] = [];
-    type = 'donut';
-    data = {
-        'Brunei Darussalam': 739,
-        Jordan: 763,
-        'Burkina Faso': 604,
-        'Cook Islands': 509,
-        Monaco: 205,
-        Switzerland: 108,
-    };
-    options = {
-        innerRadius: '70%',
-        legend: {
-            itemSelectable: true,
-            onItemClick: (e: any) => {
-                console.log(e);
-                this.selectedItems = e.chart.legend.selectedItems;
-            }
+  selectedItems: any[] = [];
+  type = 'donut';
+  data = {
+    'Brunei Darussalam': 739,
+    Jordan: 763,
+    'Burkina Faso': 604,
+    'Cook Islands': 509,
+    Monaco: 205,
+    Switzerland: 108,
+  };
+  defaultOptions = {
+    innerRadius: '70%',
+    centerLabels: [
+      {},
+      {
+        text: 'Total',
+      },
+    ],
+    legend: {
+      markerStyle: 'circle',
+    }
+  };
+  options = {
+    innerRadius: '70%',
+    centerLabels: [
+      {},
+      {
+        text: 'Total',
+        font: {
+          size: 16,
         }
-    };
+      },
+    ],
+    legend: {
+      itemSelectable: true, // TODO: remove as it has been removed in next version
+      selectable: true,
+      onItemClick: (e: any) => {
+        console.log(e);
+        this.selectedItems = e.context.chart.legend.selectedItems;
+      },
+    },
+  };
 
-    googleData = [["Month", "Days"], ["Jan", 31]];
+  googleData = {
+    cols: [{"label": "Country", "type": "string"},{"label": "Visits", "type": "number"}],
+    rows: [ ["Brunei Darussalam", 739],["Jordan", 763],["Burkina Faso", 604],["Cook Islands", 509],["Monaco", 205],["Switzerland", 108] ],
+  }
 }
-
